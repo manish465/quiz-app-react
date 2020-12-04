60fd21ae2a5b20a22406ed1e8ef8fa7308be412a
manish465 <ms5978307@gmail.com>
2020-12-03 23:50:01 -0800

    added light and dark theme

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -4,11 +4,30 @@ import { Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 
+import { ThemeProvider, createMuiTheme, CssBaseline } from "@material-ui/core";
+
+const darkTheme = createMuiTheme({
+    palette: {
+        background: { default: "#000000" },
+        text: { primary: "#ffffff" },
+    },
+});
+
+const lightTheme = createMuiTheme({
+    palette: {
+        background: { default: "#ffffff" },
+        text: { primary: "#000000" },
+    },
+});
+
 const App = () => {
     return (
-        <Route exact path='/'>
-            <HomePage />
-        </Route>
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <Route exact path='/'>
+                <HomePage />
+            </Route>
+        </ThemeProvider>
     );
 };
 
