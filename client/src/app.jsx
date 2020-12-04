7c99e7faaa3d14632cb950c15c00a359a8a323bf
manish465 <ms5978307@gmail.com>
2020-12-03 22:40:24 -0800

    added routes

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+import { Route } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+
 const App = () => {
-    return <div>Hello world</div>;
+    return (
+        <Route exact path='/'>
+            <HomePage />
+        </Route>
+    );
 };
 
 export default App;
