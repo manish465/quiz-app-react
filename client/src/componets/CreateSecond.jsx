8d9de99dac7da9aacdda790dcd8837e1b0f808fb
manish465 <ms5978307@gmail.com>
2020-12-12 05:56:16 -0800

    genrate questions by number

--- client/src/componets/CreateSecond.jsx
+++ client/src/componets/CreateSecond.jsx
@@ -2,38 +2,57 @@ import React from "react";
 
 import { TextField, Paper, Checkbox } from "@material-ui/core";
 
+const options = ["First", "Second", "Third", "Fourth"];
+
 const CreateSecond = ({
     upperMargin,
     testData,
     setTestData,
     testNumberOfQuestions,
 }) => {
-    return (
-        <>
+    const questions = Array.from(Array(parseInt(testNumberOfQuestions))).map(
+        (e, i) => (
+            <>
+                <TextField
+                    className={upperMargin}
+                    variant='outlined'
+                    label='Enter Qustion'
+                    fullWidth
+                />
+                {options.map((option, key) => (
+                    <Paper key={key} className={upperMargin}>
+                        <TextField
+                            variant='filled'
+                            label={`Enter ${option} Option`}
+                        />
+                        <Checkbox />
+                    </Paper>
+                ))}
+            </>
+        ),
+    );
+
+    return <>{questions}</>;
+};
+
+export default CreateSecond;
+
+//{
+/* <>
             <TextField
                 className={upperMargin}
                 variant='outlined'
                 label='Enter Qustion'
                 fullWidth
             />
-            <Paper>
-                <TextField variant='filled' label='Enter Option'></TextField>
-                <Checkbox size='large' />
-            </Paper>
-            <Paper>
-                <TextField variant='filled' label='Enter Option' />
-                <Checkbox size='large' />
-            </Paper>
-            <Paper>
-                <TextField variant='filled' label='Enter Option' />
-                <Checkbox size='large' />
-            </Paper>
-            <Paper>
-                <TextField variant='filled' label='Enter Option' />
-                <Checkbox size='large' />
-            </Paper>
-        </>
-    );
-};
-
-export default CreateSecond;
+            {options.map((option, key) => (
+                <Paper key={key} className={upperMargin}>
+                    <TextField
+                        variant='filled'
+                        label={`Enter ${option} Option`}
+                    />
+                    <Checkbox />
+                </Paper>
+            ))}
+        </> */
+//}
