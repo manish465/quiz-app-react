5275bf193eb21d3da289baf14d75f87768e04e37
manish465 <ms5978307@gmail.com>
2020-12-13 03:27:42 -0800

    basic server added

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const port = 8000 || process.env.PORT;
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+server.listen(port, () => console.log(`server is running on ${port}`));
