c4413cafe31eab05cafbb94bab37c2c197189865
manish465 <ms5978307@gmail.com>
2020-12-14 21:24:42 -0800

    added DB

---
+++ server/models/createTest.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose");
+
+const TestSchema = mongoose.Schema({
+    title: String,
+});
--- server/server.js
+++ server/server.js
@@ -3,9 +3,21 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const port = 8000 || process.env.PORT;
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv/config");
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
+
+mongoose.connect(
+    process.env.URL,
+    { useNewUrlParser: true },
+    { useUnifiedTopology: true },
+    () => console.log("Connection established"),
+);
 
 server.listen(port, () => console.log(`server is running on ${port}`));
