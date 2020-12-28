3862cebdbaf8ea0d82d28a11d8e12add900155ad
manish465 <ms5978307@gmail.com>
2020-12-28 03:25:39 -0800

    added page buttons

--- client/src/pages/AttemptPage.jsx
+++ client/src/pages/AttemptPage.jsx
@@ -1,6 +1,16 @@
 import React, { useState, useEffect } from "react";
 
-import { Paper, Typography, makeStyles } from "@material-ui/core";
+import {
+    Paper,
+    Typography,
+    makeStyles,
+    List,
+    ListItem,
+    ListItemText,
+    Divider,
+    ButtonGroup,
+    Button,
+} from "@material-ui/core";
 
 import axios from "axios";
 import { useParams } from "react-router-dom";
@@ -11,11 +21,11 @@ const endpoint = "/api/tests/";
 const useStyles = makeStyles((theme) => ({
     mainDiv: {
         height: "100vh",
-        padding: "100px 70px",
+        padding: theme.spacing(10),
     },
     mainPaper: {
         backgroundColor: theme.palette.secondary.main,
-        padding: "30px 40px",
+        padding: theme.spacing(3),
         paddingTop: "5px",
         borderRadius: "20px",
         display: "flex",
@@ -24,7 +34,7 @@ const useStyles = makeStyles((theme) => ({
     },
     question: {
         backgroundColor: theme.palette.primary.main,
-        padding: "20px 50px",
+        padding: theme.spacing(2),
         borderRadius: "20px",
         position: "relative",
     },
@@ -38,6 +48,20 @@ const useStyles = makeStyles((theme) => ({
         top: "15px",
         zIndex: "100",
         boxShadow: theme.shadows[11],
+    },
+    options: {
+        marginTop: theme.spacing(3),
+        padding: theme.spacing(1),
+        backgroundColor: theme.palette.primary.main,
+        borderRadius: "20px",
+    },
+    option: {
+        height: "40px",
+        textAlign: "center",
+        padding: theme.spacing(1),
+    },
+    page: {
+        marginTop: theme.spacing(3),
     },
 }));
 
@@ -55,8 +79,6 @@ const AttemptPage = () => {
         });
     }, [params.id]);
 
-    console.log(data);
-
     return (
         <div className={classes.mainDiv}>
             <Paper variant='outlined' className={classes.mainPaper}>
@@ -70,6 +92,34 @@ const AttemptPage = () => {
                         {data ? data.testData[number].text : "Loading..."}
                     </Typography>
                 </Paper>
+                <List className={classes.options}>
+                    {data
+                        ? data.testData[number].options.map((option, key) => (
+                              <div key={key}>
+                                  <ListItem className={classes.option} button>
+                                      <ListItemText primary={option.text} />
+                                  </ListItem>
+                                  <Divider />
+                              </div>
+                          ))
+                        : "Loading..."}
+                </List>
+                {data ? (
+                    <ButtonGroup className={classes.page} fullWidth>
+                        <Button
+                            onClick={() => setNumber(number - 1)}
+                            disabled={number === 0}>
+                            Back
+                        </Button>
+                        <Button
+                            onClick={() => setNumber(number + 1)}
+                            disabled={number + 1 === data.testData.length}>
+                            Next
+                        </Button>
+                    </ButtonGroup>
+                ) : (
+                    "Loading..."
+                )}
             </Paper>
         </div>
     );
