3dc76f6ad18ed0e9043ed47343d7f4e8d618b817
manish465 <ms5978307@gmail.com>
2020-12-28 22:00:10 -0800

    added sing up

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -7,21 +7,20 @@ import {
     Tabs,
     Tab,
     makeStyles,
+    Grid,
 } from "@material-ui/core";
 
 import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
     mainDiv: {
-        padding: "150px 200px",
+        padding: "100px 150px",
     },
     pagePaper: {
         padding: "30px",
         backgroundColor: theme.palette.secondary.main,
         borderRadius: "5px",
     },
-    loginTextFeid: {},
-    loginPageSaveButton: { marginTop: "10px" },
 }));
 
 const TabPanel = (props) => {
@@ -44,24 +43,95 @@ const LoginPage = ({ userName, setUserName }) => {
                 <Tab label='Login' />
             </Tabs>
             <TabPanel value={value} index={0}>
-                icjq
+                <Paper className={classes.pagePaper}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={6}>
+                            <TextField
+                                fullWidth
+                                name='firstName'
+                                variant='outlined'
+                                label='Enter Your First Name'
+                            />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField
+                                fullWidth
+                                name='lastName'
+                                variant='outlined'
+                                label='Enter Your Last Name'
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                fullWidth
+                                name='email'
+                                type='email'
+                                variant='outlined'
+                                label='Enter Your Email'
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                fullWidth
+                                name='password_1'
+                                type='password'
+                                variant='outlined'
+                                label='Create A Password'
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                fullWidth
+                                name='password_2'
+                                type='password'
+                                variant='outlined'
+                                label='Re-enter Your Password'
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Button
+                                component={Link}
+                                to='/'
+                                color='primary'
+                                fullWidth
+                                variant='contained'>
+                                Sign up
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Paper>
             </TabPanel>
             <TabPanel value={value} index={1}>
                 <Paper className={classes.pagePaper}>
-                    <TextField
-                        fullWidth
-                        variant='outlined'
-                        label='Enter Your User Name'
-                    />
-                    <Button
-                        component={Link}
-                        to='/'
-                        className={classes.loginPageSaveButton}
-                        color='primary'
-                        fullWidth
-                        variant='contained'>
-                        SAVE
-                    </Button>
+                    <Grid container spacing={2}>
+                        <Grid item xs={6}>
+                            <TextField
+                                fullWidth
+                                name='userName'
+                                variant='outlined'
+                                label='Enter Your User Name'
+                            />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField
+                                fullWidth
+                                type='password'
+                                name='password'
+                                variant='outlined'
+                                label='Enter Your Password'
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Button
+                                component={Link}
+                                to='/'
+                                color='primary'
+                                fullWidth
+                                variant='contained'>
+                                Login
+                            </Button>
+                        </Grid>
+                    </Grid>
                 </Paper>
             </TabPanel>
         </div>
