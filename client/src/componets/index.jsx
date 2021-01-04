2ca51117a4aadb78160e5206143f46f66915e8d3
manish465 <ms5978307@gmail.com>
2021-01-04 01:41:17 -0800

    new create page

--- client/src/componets/index.jsx
+++ client/src/componets/index.jsx
@@ -1,21 +1,8 @@
 import AppBarContent from "./AppBarContent";
-import FirstStep from "./FirstStep";
 import Header from "./Header";
 import HeaderMenu from "./HeaderMenu";
-import SecondStep from "./SecondStep";
 import SvgBanner from "./SvgBanner";
-import ThirdStep from "./ThirdStep";
 import Login from "./Login";
 import SingUp from "./SingUp";
 
-export {
-    AppBarContent,
-    FirstStep,
-    Header,
-    HeaderMenu,
-    SecondStep,
-    SvgBanner,
-    ThirdStep,
-    Login,
-    SingUp,
-};
+export { AppBarContent, Header, HeaderMenu, SvgBanner, Login, SingUp };
--- client/src/pages/CreatePage.jsx
+++ client/src/pages/CreatePage.jsx
@@ -1,14 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 import {
     makeStyles,
     Button,
     Paper,
-    Divider,
     Grid,
-    Stepper,
-    Step,
-    StepLabel,
     TextField,
     List,
     ListItem,
@@ -35,8 +31,6 @@ const useStyles = makeStyles((theme) => ({
 const CreatePage = () => {
     const classes = useStyles();
 
-    const [activeStep, setActiveStep] = useState(0);
-
     const { control, register, handleSubmit } = useForm({
         defaultValues: {
             name: "",
@@ -60,11 +54,11 @@ const CreatePage = () => {
         name: "testQuestion",
     });
 
-    const getStepContent = (step) => {
-        switch (step) {
-            case 0:
-                return (
-                    <>
+    return (
+        <div className={classes.createPageMaindiv}>
+            <Paper className={classes.createPagePaper}>
+                <form onSubmit={handleSubmit((data) => console.log(data))}>
+                    <Grid container spacing={2}>
                         <Grid item xs={12}>
                             <TextField
                                 inputRef={register}
@@ -85,162 +79,102 @@ const CreatePage = () => {
                                 name='description'
                             />
                         </Grid>
-                    </>
-                );
-            case 1:
-                return fields.map((field, index) => (
-                    <React.Fragment key={field.id}>
-                        <Grid item xs={12}>
-                            <TextField
-                                fullWidth
-                                inputRef={register}
-                                variant='outlined'
-                                placeholder='Enter The Question'
-                                label='Question'
-                                name={`testQuestion[${index}].text`}
-                                defaultValue={field.text}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <List>
-                                <ListItem>
-                                    <Checkbox
-                                        inputRef={register}
-                                        name={`testQuestion[${index}].options[0].answer`}
-                                    />
+                        {fields.map((field, index) => (
+                            <React.Fragment key={field.id}>
+                                <Grid item xs={12}>
                                     <TextField
+                                        fullWidth
                                         inputRef={register}
                                         variant='outlined'
-                                        placeholder='Enter Option'
-                                        label='Option'
-                                        name={`testQuestion[${index}].options[0].text`}
+                                        placeholder='Enter The Question'
+                                        label='Question'
+                                        name={`testQuestion[${index}].text`}
                                     />
-                                </ListItem>
-                                <ListItem>
-                                    <Checkbox
-                                        inputRef={register}
-                                        name={`testQuestion[${index}].options[1].answer`}
-                                    />
-                                    <TextField
-                                        inputRef={register}
-                                        variant='outlined'
-                                        placeholder='Enter Option'
-                                        label='Option'
-                                        name={`testQuestion[${index}].options[1].text`}
-                                    />
-                                </ListItem>
-                                <ListItem>
-                                    <Checkbox
-                                        inputRef={register}
-                                        name={`testQuestion[${index}].options[2].answer`}
-                                    />
-                                    <TextField
-                                        inputRef={register}
-                                        variant='outlined'
-                                        placeholder='Enter Option'
-                                        label='Option'
-                                        name={`testQuestion[${index}].options[2].text`}
-                                    />
-                                </ListItem>
-                                <ListItem>
-                                    <Checkbox
-                                        inputRef={register}
-                                        name={`testQuestion[${index}].options[3].answer`}
-                                    />
-                                    <TextField
-                                        inputRef={register}
-                                        variant='outlined'
-                                        placeholder='Enter Option'
-                                        label='Option'
-                                        name={`testQuestion[${index}].options[3].text`}
-                                    />
-                                </ListItem>
-                            </List>
-                            <Button
-                                className={classes.stepButton}
-                                color='primary'
-                                variant='contained'
-                                onClick={append}>
-                                Add Question
-                            </Button>
-                            <Button
-                                className={classes.stepButton}
-                                color='primary'
-                                variant='contained'
-                                onClick={() => remove(index)}>
-                                Remove Aboved Question
-                            </Button>
-                        </Grid>
-                    </React.Fragment>
-                ));
-            case 2:
-                return (
-                    <Button
-                        size='large'
-                        type='submit'
-                        color='primary'
-                        variant='contained'>
-                        Publish
-                    </Button>
-                );
-            default:
-                return "Unknown step";
-        }
-    };
-
-    return (
-        <div className={classes.createPageMaindiv}>
-            <Stepper activeStep={activeStep}>
-                <Step>
-                    <StepLabel>1</StepLabel>
-                </Step>
-                <Step>
-                    <StepLabel>2</StepLabel>
-                </Step>
-                <Step>
-                    <StepLabel>3</StepLabel>
-                </Step>
-            </Stepper>
-            <Paper className={classes.createPagePaper}>
-                <form onSubmit={handleSubmit((data) => console.log(data))}>
-                    <Grid container spacing={2}>
-                        {getStepContent(activeStep)}
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <List>
+                                        <ListItem>
+                                            <Checkbox
+                                                inputRef={register}
+                                                name={`testQuestion[${index}].options[0].answer`}
+                                            />
+                                            <TextField
+                                                inputRef={register}
+                                                variant='outlined'
+                                                placeholder='Enter Option'
+                                                label='Option'
+                                                name={`testQuestion[${index}].options[0].text`}
+                                            />
+                                        </ListItem>
+                                        <ListItem>
+                                            <Checkbox
+                                                inputRef={register}
+                                                name={`testQuestion[${index}].options[1].answer`}
+                                            />
+                                            <TextField
+                                                inputRef={register}
+                                                variant='outlined'
+                                                placeholder='Enter Option'
+                                                label='Option'
+                                                name={`testQuestion[${index}].options[1].text`}
+                                            />
+                                        </ListItem>
+                                        <ListItem>
+                                            <Checkbox
+                                                inputRef={register}
+                                                name={`testQuestion[${index}].options[2].answer`}
+                                            />
+                                            <TextField
+                                                inputRef={register}
+                                                variant='outlined'
+                                                placeholder='Enter Option'
+                                                label='Option'
+                                                name={`testQuestion[${index}].options[2].text`}
+                                            />
+                                        </ListItem>
+                                        <ListItem>
+                                            <Checkbox
+                                                inputRef={register}
+                                                name={`testQuestion[${index}].options[3].answer`}
+                                            />
+                                            <TextField
+                                                inputRef={register}
+                                                variant='outlined'
+                                                placeholder='Enter Option'
+                                                label='Option'
+                                                name={`testQuestion[${index}].options[3].text`}
+                                            />
+                                        </ListItem>
+                                    </List>
+                                    <Button
+                                        className={classes.stepButton}
+                                        color='primary'
+                                        variant='contained'
+                                        onClick={append}>
+                                        Add Question
+                                    </Button>
+                                    <Button
+                                        className={classes.stepButton}
+                                        color='primary'
+                                        variant='contained'
+                                        onClick={() => remove(index)}>
+                                        Remove Aboved Question
+                                    </Button>
+                                </Grid>
+                            </React.Fragment>
+                        ))}
+                        <Button
+                            size='large'
+                            type='submit'
+                            color='primary'
+                            variant='contained'>
+                            Publish
+                        </Button>
                     </Grid>
                 </form>
-                <Divider className={classes.upperMargin} />
-                <Button
-                    onClick={() =>
-                        setActiveStep((activeStep) => activeStep - 1)
-                    }
-                    disabled={activeStep === 0}
-                    className={classes.stepButton}
-                    color='primary'
-                    variant='contained'>
-                    Back
-                </Button>
-                <Button
-                    onClick={() =>
-                        setActiveStep((activeStep) => activeStep + 1)
-                    }
-                    disabled={activeStep === 2}
-                    className={classes.stepButton}
-                    color='primary'
-                    variant='contained'>
-                    Next
-                </Button>
             </Paper>
         </div>
     );
 };
 
 export default CreatePage;
-
-/* <Button
-                        className={classes.upperMargin}
-                        onClick={() => console.log(testData)}
-                        disabled={activeStep !== 2}
-                        size='large'
-                        color='primary'
-                        variant='contained'>
-                        Submit
-                    </Button> */
