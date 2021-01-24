bb913cb8485358c02eadf22bcc7bdc0ff6f32090
manish465 <ms5978307@gmail.com>
2021-01-23 21:48:29 -0800

    checking for exiting email and encryping password

--- server/routes/Auth.js
+++ server/routes/Auth.js
@@ -1,13 +1,19 @@
 const router = require("express").Router();
-
+const bcrypt = require("bcryptjs");
 const User = require("../model/User");
 
 router.post("/register", async (req, res) => {
+    const emailExists = await User.findOne({ email: req.body.email });
+    if (emailExists) return res.status(400).send("email Exist");
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
     const user = new User({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
-        password: req.body.password,
+        password: hashedPassword,
     });
 
     try {
